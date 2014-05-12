917c70f4acd8f7ddd7c3ae64e207dd54c626b65b
Ery Lee <ery.lee@gmail.com>
2014-05-12 16:16:33 +0800

    up static

--- static/images/upload.php
+++ static/images/upload.php
@@ -13,8 +13,9 @@ class WebimUploadHandler extends UploadHandler
 {
 	protected function get_file_name($name,
 		$type = null, $index = null, $content_range = null) {
+			$ext = pathinfo($name, PATHINFO_EXTENSION);
 			return $this->get_unique_filename(
-				$this->trim_file_name(str_replace('.', '-', microtime(true) . "." . rand(100, 999)), $type, $index, $content_range),
+				$this->trim_file_name(str_replace('.', '-', microtime(true) . "." . rand(100, 999)) . ($ext ? ("." . $ext) : ""), $type, $index, $content_range),
 				$type,
 				$index,
 				$content_range
@@ -23,7 +24,8 @@ class WebimUploadHandler extends UploadHandler
 
 	protected function handle_file_upload($uploaded_file, $name, $size, $type, $error,
 		$index = null, $content_range = null) {
-			$file  = call_user_func_array(array($this, 'parent::handle_file_upload'), func_get_args());
+            $args = func_get_args();
+			$file  = call_user_func_array(array($this, 'parent::handle_file_upload'), $args);
 			$file->name = $this->trim_file_name($name, $type, $index, $content_range);
 			return $file;
 		}
--- static/webim.jinhusns.js
+++ static/webim.jinhusns.js
@@ -14,51 +14,68 @@
 		history: path + "History" + aspx,
         clear: path + "ClearHistory" + aspx,
         download: path + "DownloadHistory" + aspx,
-        members: path + "Members" + aspx,
-        join: path + "Join" + aspx,
-        leave: path + "Leave" + aspx,
+        //room actions
+		join: path + "Join" + aspx,
+		leave: path + "Leave" + aspx,
+		block: path + "Block" + aspx,
+		unblock: path + "Unblock" + aspx,
+		members: path + "Members" + aspx,
 		buddies: path + "Buddies" + aspx,
-		notifications: path + "Notifications" + aspx
+        //notifications
+		notifications: path + "Notifications" + aspx,
+        //upload files
+		upload: path + "Upload" + aspx
 	});
-
-    webim.ui.emot.init({ "dir": _IMC.uiPath + "static/images/emot/default" });
-    var soundUrls = {
-        lib: _IMC.uiPath + "static/assets/sound.swf",
-        msg: _IMC.uiPath + "static/assets/sound/msg.mp3"
-    };
-    var ui = new webim.ui(document.body, {
-        imOptions: {
-            jsonp: _IMC.jsonp
+	webim.ui.emot.init({ "dir": "static/images/emot/default" });
+	var soundUrls = {
+		lib: "static/assets/sound.swf",
+		msg: "static/assets/sound/msg.mp3"
+	};
+	var ui = new webim.ui(document.body, {
+		imOptions: {
+			jsonp: _IMC.jsonp
+		},
+		soundUrls: soundUrls,
+		//layout: "layout.popup",
+        layoutOptions: {
+            unscalable: _IMC.is_visitor
         },
         soundUrls: soundUrls,
 		buddyChatOptions: {
-			upload: _IMC.upload
+            downloadHistory: !_IMC.is_visitor,
+			//simple: _IMC.is_visitor,
+			upload: _IMC.upload && !_IMC.is_visitor
 		},
 		roomChatOptions: {
+            downloadHistory: !_IMC.is_visitor,
 			upload: _IMC.upload
 		}
     }), im = ui.im;
+    //全局化
+    window.webimUI = ui;
 
 	if( _IMC.user ) im.setUser( _IMC.user );
-    //if( _IMC.menu ) ui.addApp("menu", { "data": _IMC.menu } );
-    if (_IMC.enable_shortcut) ui.layout.addShortcut(_IMC.menu);
+	if( _IMC.menu ) ui.addApp("menu", { "data": _IMC.menu } );
+	if( _IMC.enable_shortcut ) ui.layout.addShortcut( _IMC.menu );
 
     ui.addApp("buddy", {
 		showUnavailable: _IMC.show_unavailable,
 		is_login: _IMC['is_login'],
-		collapse: false,
 		disable_login: true,
-        loginOptions: _IMC['login_options']
+		collapse: false,
+		//disable_user: _IMC.is_visitor,
+        //simple: _IMC.is_visitor,
+		loginOptions: _IMC['login_options']
     });
-	if( _IMC.enable_room )ui.addApp("room", { discussion: false});
+    if( _IMC.enable_room )ui.addApp("room", { discussion: (_IMC.discussion && !_IMC.is_visitor) });
 	if( _IMC.enable_noti )ui.addApp("notification");
-    ui.addApp("setting", { "data": webim.setting.defaults.data });
     if (_IMC.enable_chatlink) ui.addApp("chatlink", {
         space_href: [/mod=space&uid=(\d+)/i, /space\-uid\-(\d+)\.html$/i],
         space_class: /xl\sxl2\scl/,
         space_id: null,
         link_wrap: document.getElementById("ct")
     });
+    ui.addApp("setting", {"data": webim.setting.defaults.data, "copyright": true});
     ui.render();
     _IMC['is_login'] && im.autoOnline() && im.online();
 })(webim);
